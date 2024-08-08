a6cc650d80aab317a692c81f43c8e32909bfad46
Bruno Caldeira <127410564+caldeira-bruno@users.noreply.github.com>
2024-08-08 18:45:57 -0300

    Update mysql.mjs

--- database/mysql.mjs
+++ database/mysql.mjs
@@ -1,6 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("mysql://root:root@localhost:3306/clientesdb");
+//const sequelize = new Sequelize("mysql://root:root@localhost:3306/clientesdb");
+const sequelize = new Sequelize("postgresql://user_db:iSIQToo5c2ziV6eq2EJo8rV6FL8CLfBX@dpg-cqq1ffggph6c7382vbug-a/clients_eq4k");
 sequelize.sync();
 
 export default sequelize;
