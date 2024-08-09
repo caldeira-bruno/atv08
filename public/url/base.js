9e0f308a3b3801861537b20b79ac57fcf4a3f280
Bruno Caldeira <127410564+caldeira-bruno@users.noreply.github.com>
2024-08-09 19:16:58 -0300

    Update base.js

--- public/url/base.js
+++ public/url/base.js
@@ -2,8 +2,3 @@ const urlBaseAPI = 'https://atv08.onrender.com';
 const urlBaseFront = 'https://atv08.onrender.com';
 
 export { urlBaseAPI, urlBaseFront };
-
-// const urlBaseAPI = 'http://localhost:3000';
-// const urlBaseFront = 'http://localhost:5500/atividade07_front';
-
-// export { urlBaseAPI, urlBaseFront };
