8c902ada0ccb91e61236d864baec7b1b6764b115
Bruno Caldeira <127410564+caldeira-bruno@users.noreply.github.com>
2024-08-09 19:35:46 -0300

    Update events.js

--- public/registro/events.js
+++ public/registro/events.js
@@ -1,7 +1,7 @@
-import { realizaLogin } from './login.js';
+import { realizaRegistro } from './registro.js';
 
 const btLogin = document.querySelector('form button');
 btLogin.addEventListener('click', (e) => {
     e.preventDefault();
-    realizaLogin();
+    realizaRegistro();
 });
